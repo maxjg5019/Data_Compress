7368ba8a431d818e8116baae822a8d5bbe99e325
maxjg5019 <maxjg5019@gmail.com>
2023-01-11 17:17:23 +0800

    fin

--- LZW.py
+++ LZW.py
@@ -1,8 +1,6 @@
 def compress(uncompressed):
-    """將輸入字串壓縮成符號 List """
-
-    # 產生初始編碼字典 0~255
-    dict_size = 256
+    # 產生初始編碼字典 0~127
+    dict_size = 128
     dictionary = {chr(i): i for i in range(dict_size)}
 
     # 初始化，w為空字元，result為空串列
@@ -15,27 +13,23 @@ def compress(uncompressed):
             w = wc
         else:
             result.append(dictionary[w])
-            # Add wc to the dictionary.
+            # 把新的符號(wc)加到字典中.
             dictionary[wc] = dict_size
             dict_size += 1
             w = c
 
-    # Output the code for w.
+    # 記得要把最後一個符號丟進字串裡面，我還沒上車R。
     if w:
         result.append(dictionary[w])
     return result
 
 
 def decompress(compressed):
-    """Decompress a list of output ks to a string."""
     from io import StringIO
-
-    # 產生初始編碼字典 0~255
-    dict_size = 256
+    # 產生初始編碼字典 0~127
+    dict_size = 128
     dictionary = {i: chr(i) for i in range(dict_size)}
 
-    # use StringIO, otherwise this becomes O(N^2)
-    # due to string concatenation in a loop
     result = StringIO()
     w = chr(compressed.pop(0))
     result.write(w)
@@ -48,7 +42,7 @@ def decompress(compressed):
             raise ValueError('Bad compressed k: %s' % k)
         result.write(entry)
 
-        # Add w+entry[0] to the dictionary.
+        # 把新的符號(w+entry[0])加到字典中.
         dictionary[dict_size] = w + entry[0]
         dict_size += 1
 
@@ -56,8 +50,8 @@ def decompress(compressed):
     return result.getvalue()
 
 
-# 壓縮它 ! 先執行編碼，再解碼。:
-compressed = compress('ABCCAABCDDAACCDB')
+# 壓縮它 ! 先編碼，再解碼。
+compressed = compress('ABABABABAB')
 print (compressed)
 decompressed = decompress(compressed)
 print (decompressed)
