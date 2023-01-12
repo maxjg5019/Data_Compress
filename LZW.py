2bd5778bc125d490dbaf7bfe2d04b4ce43016dd1
maxjg5019 <maxjg5019@gmail.com>
2023-01-12 16:43:31 +0800

    Finish

--- LZW.py
+++ LZW.py
@@ -20,7 +20,7 @@ def compress(uncompressed):
             # print(Dictionary[Previous])
             # 把新的符號(PC)加到字典中.
             Dictionary[PC] = Dict_size
-            print("新增 " + PC + "到字典並編碼:" + str(Dict_size) )
+            print("新增 " + PC + "到字典並編碼:" + str(Dict_size))
             Dict_size += 1
             Previous = Current
             print("更新:Previous =  " + Previous)
@@ -42,26 +42,30 @@ def decompress(compressed):
     print("Cw讀取第一個符號: " + str(compressed[0]))
     Word = chr(compressed.pop(0))
     print("解碼輸出Cw(): " + Word)
+    print("----------")
     Result.write(Word)
     for Code in compressed:
         if Code in Dictionary:
             Entry = Dictionary[Code]
+            print("Cw有找到,解碼當前Cw: " + str(Code) +
+             " 輸出Cw(): " + Entry)
         elif Code == Dict_size:
             Entry = Word + Word[0]
-        # else:
-        #     raise ValueError('Bad compressed k: %s' % Code)
+            # 這邊需要通靈一下，按照編碼邏輯才能篤定解碼時直接輸出
+            print("Cw沒找到,紀錄當前符號 " + 
+            Entry + " 為: " + str(Code) + " 同時直接輸出P+C: " + Entry)
         Result.write(Entry)
-
         # 把新的符號(Word+Entry[0])加到字典中.
         Dictionary[Dict_size] = Word + Entry[0]
+        print("新增P+C符號: " + str(Dictionary[Dict_size]))
         Dict_size += 1
         Word = Entry
-    print("----------")    
+        print("----------")    
     return Result.getvalue()
 
 
 # 壓縮它 ! 先編碼，再解碼。
-compressed = compress('ABABBA')
+compressed = compress('ABABBA') # ABABCABABAC 可測試通靈那部分
 print (compressed)
 print("編碼結束")
 print()
